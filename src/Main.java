06ad1ebe614b8448a1aed514218da237fde15d58
Janna <jannasheyanova@gmail.com>
2023-06-26 14:29:28 +0300

    Create class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int amount = 11_430;
-        int mile = 20;
-        int bonus = amount / mile;
-        System.out.println(bonus);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
