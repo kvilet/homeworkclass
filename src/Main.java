7f8b513d5b879cd595741c298083dc5ace594afa
Janna <jannasheyanova@gmail.com>
2023-06-26 14:13:58 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 11_430;
+        int mile = 20;
+        int bonus = amount / mile;
+        System.out.println(bonus);
+    }
+}
